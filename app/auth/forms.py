a6bb4aec059b9a086369069fc75089953927826e
David Schommer <djschomes@gmail.com>
2018-02-10 15:59:53 -0600

    update login form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email
+from wtforms.validators import ValidationError, DataRequired
 
 # TODO: ValidationErrors, registration, forgot password
 class LoginForm(FlaskForm):
-	email = StringField('Email', validators=[DataRequired(), Email()])
+	username = StringField('Username', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	submit = SubmitField('Sign in')
