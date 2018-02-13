aed6955d39ac181796a4af74ee67bea7fa3de19f
Huels017 <huels017@umn.edu>
2018-02-13 08:32:16 -0600

    CSH/issue-30/Clean-UP-Master

--- App-V-0.1/Run.py
+++
@@ -1,9 +0,0 @@
-from Asset_Allocation_2 import resourceAllocation
-
-
-
-#resourceAllocation('Allocation_Template_2.xlsx')
-
-resourceAllocation('Allocation_Template_2_Cole.xlsx')
-
-#resourceAllocation('Allocation_Template_2_Hurley.xlsx')
