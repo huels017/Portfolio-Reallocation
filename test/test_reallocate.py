693f346e91507344d4db6f5383a9d97c5deafeed
Huels017 <huels017@umn.edu>
2018-03-06 10:52:51 -0600

    delete print statement

--- test/test_reallocate.py
+++ test/test_reallocate.py
@@ -53,7 +53,6 @@ class TestReallocateFunctions(unittest.TestCase):
     def test_difference_current_desired_allocation(self):
         expectedDictionary = {u'REIT': 2157.432200000001, u'Muni Bonds': 3443.024899999993, u'Emg Mkts': 46393.18639999999, u'Cryptocurrency': 0.0, u'Cash/MMKT': -67491.6868, u'Commodities': 1456.4422000000004, u'Sm/Mid Growth': 1887.2531999999992, u'LC Value': 44294.884000000005, u'Tax Bonds': 31047.504899999996, u'LC Growth': -69572.89799999999, u'Balanced': -45000.0, u'International': 85424.29959999998, u'LC Blend': -27601.43800000001, u'Sm/Mid Value': 1162.7927000000018, u'Sm/Mid Blend': -7600.797299999995}
         actualDictionary = differenceCurrentDesiredAccounts(self.accounts, self.desiredAllocation)
-        print(actualDictionary)
         self.assertEqual(actualDictionary, expectedDictionary)
         #list of categories and diff value per category
 
